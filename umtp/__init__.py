d43a234b4db9a1a71ae0697f9a5f49e9fc71c94b
Saksham dhakad <sakshamdhakad98@gmail.com>
2026-01-06 15:07:11 +0530

    Update __init__.py

--- umtp/__init__.py
+++ umtp/__init__.py
@@ -2,8 +2,8 @@
 # Version 0.1.0
 
 from .protocol import UMTPPacket
-from .client import send_matrix
-from .server import start_server
+from .sender import send_matrix
+from .receiver import start_server
 
 __version__ = "0.1.0"
 __author__ = "Saksham Dhakad"
