8a01c1bafb3b2597f1812369d5f5f183fd85a0f3
Saksham dhakad <sakshamdhakad98@gmail.com>
2026-01-06 15:08:42 +0530

    Update sender.py

--- umtp/sender.py
+++ umtp/sender.py
@@ -3,7 +3,7 @@ import numpy as np
 import time
 from protocol import UMTPPacket
 
-def send_consciousness(host='127.0.0.1', port=9999):
+def send_matrix(host='127.0.0.1', port=9999):
     try:
         # 1. Generate Artificial Data (Simulating a Neural Layer output)
         # Let's create a 3D Tensor (Batch, Time, Features)
@@ -32,4 +32,4 @@ def send_consciousness(host='127.0.0.1', port=9999):
         print("[!] Error: Target Node is offline.")
 
 if __name__ == "__main__":
-    send_consciousness()
+    send_matrix()
